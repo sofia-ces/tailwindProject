5a2926dc28c9ce0749f7811b168ad8cc1db49ef8
Cecilia Salenga <csalenga@intercommerce.com.ph>
2021-08-15 19:35:10 +0800

    Alll

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
